d85412f636e22ae95d702678954e2e1aa7700bf9
Evan Mildenberger <emildenberger>
2020-04-11 20:38:54 +0300

    Modify mnist_reader.py.
    'Various functions needed to be changed in order for the reader to work properly.'

--- mnist_reader.py
+++ mnist_reader.py
@@ -13,12 +13,12 @@ def _to_int(byte_string, is_signed=False):
     except TypeError:
         return byte_string
 
-def _read_labels(byte_string):
+def _read_labels(byte_string, chunks):
     # Return a list of ints (0 through 9) given a byte string.
-    return [_to_int(byte_char) for byte_char in byte_string]
+    return [_to_int(byte_string[chunk]) for chunk in range(chunks)]
 
-def _read_images(byte_string, image_amount):
-    # Return a 28 by 28 by image_amount nested list given a byte string
+def _read_images(byte_string, chunks):
+    # Return a 28 by 28 by chunks nested list given a byte string
     # and an int of the amount of images.
     rows = _to_int(byte_string[:4])
     columns = _to_int(byte_string[4:8])
@@ -27,14 +27,16 @@ def _read_images(byte_string, image_amount):
         return ValueError
     # Remove the first 8 bytes as they are only meta data for the dimensions.
     byte_string = byte_string[8:]
+    # Convert the necessary number of pixel bytes into ints.
+    int_list = [byte_string[i] for i in range(chunks*rows*columns)]
     # Convert byte string into a nested list of size (images x rows x columns).
-    images = [[byte_string[j:j+columns] for j in range(0, len(byte_string), columns)][i:i+rows] for i in range(0, rows*image_amount, rows)]
-    if len(images) != image_amount:
-        return ValueError
+    images = [[int_list[j:j+columns] for j in range(0, len(int_list), columns)][i:i+rows] for i in range(0, chunks*rows, rows)]
+    if len(images) != chunks:
+        print(ValueError)
     return images
 
 
-def read_file(filename):
+def read_file(filename, chunks):
     """Return a list or nested list given a filename for the MNIST data."""
     with open(filename, "rb") as file:
         contents = file.read()
@@ -43,10 +45,10 @@ def read_file(filename):
     example_size = _to_int(meta_data[4:8])
     # This is for the labels.
     if magic_byte == 2049:
-        data_list = _read_labels(data)
+        data_list = _read_labels(data, chunks)
     # This is for the images.
     elif magic_byte == 2051:
-        data_list = _read_images(data)
+        data_list = _read_images(data, chunks)
     # An error should be raised if attempting to read in the wrong file.
     else:
         return KeyError
